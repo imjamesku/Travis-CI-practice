e6f9ead6cf1ab417038b8fa9b26208a87c5f5f8b
James <jameskutw@gmail.com>
2019-01-14 12:34:30 +0800

    Adds index page

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	// io.WriteString(res, "Hello")
+	tpl.ExecuteTemplate(res, "index.html", nil)
+}
+
+func main() {
+	http.HandleFunc("/", index)
+
+	http.ListenAndServe(":8080", nil)
+}
